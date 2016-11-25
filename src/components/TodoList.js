f21c67bf1a21fbfea19e47c73d70f3ca1c38ca86
dwicao <lutfiandwica@gmail.com>
2016-11-25 15:02:27 +0700

    separated todoItem from todoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,9 @@
 import React, { PropTypes } from 'react';
 import FilterLink from './FilterLink';
-import ToggleTodo from './ToggleTodo';
+import TodoItem from './TodoItem';
 
 const TodoList = (props) => {
-
   const { todos, actions, visibilityFilter } = props;
-
-  const _toggleClick = id => event => actions.toggleTodo(id);
-
-  const _removeTodo = index => event => actions.removeTodo(index);
-
-  const _toggleEdit = id => event => actions.toggleEditTodo(id);
 
   const getVisibleTodos = (allTodos, whatFilter) => {
     switch(whatFilter) {
@@ -25,93 +18,12 @@ const TodoList = (props) => {
           t => !t.isDone
         );
     }
-  }
+  };
 
   const visibleTodos = getVisibleTodos(
     todos,
     visibilityFilter
   );
-
-  const renderList = (currTodo, index) => {
-    const renderToggleTodo = () => (
-      <ToggleTodo
-        isDone={currTodo.isDone}
-        todoId={currTodo.id}
-        actions={actions} />
-    );
-
-    const renderButton = () => (
-      <span>
-        <button onClick={_toggleEdit(currTodo.id)}>&#8230;</button>
-        <button onClick={_removeTodo(index)}>&times;</button>
-      </span>
-    );
-
-    const _onKeyDown = event => {
-      const input = event.target;
-      const text = input.value;
-      const isEnterKey = (event.which === 13);
-      const isLongEnough = text.length > 0;
-
-      if(isEnterKey && isLongEnough) {
-        actions.editTodo(currTodo.id, text);
-        actions.toggleEditTodo(currTodo.id);
-        return (
-          <div>
-            {renderToggleTodo()}
-            <span>{currTodo.text}</span>
-            {renderButton()}
-          </div>
-        );
-      }
-    }
-
-    const _onChange = event => {
-      actions.editTodo(currTodo.id, event.target.value);
-      event.target.value = currTodo.text;
-    }
-
-    const _onApply = () => {
-      actions.toggleEditTodo(currTodo.id);
-    }
-
-    const truncateStr = (str, len) => {
-      return str.length > len ? str.substring(0, len - 3) + '...' : str;
-    }
-
-    if(currTodo.isEditing){
-      return (
-        <div>
-          <input
-            type="text"
-            onKeyDown={_onKeyDown}
-            onChange={_onChange}
-            value={currTodo.text} />
-          <button onClick={_onApply}>Apply</button>
-        </div>
-      );
-    }
-
-    if(currTodo.isDone) {
-      return (
-        <div>
-          {renderToggleTodo()}
-          <strike>{truncateStr(currTodo.text, 36)}</strike>
-          {renderButton()}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          {renderToggleTodo()}
-          <span>{truncateStr(currTodo.text, 36)}</span>
-          {renderButton()}
-        </div>
-      );  
-    }
-
-  };
-
 
   return (
     <div>
@@ -140,7 +52,10 @@ const TodoList = (props) => {
       <ul>
         {visibleTodos.map((currTodo, index) => (
           <li key={index}>
-            {renderList(currTodo, index)}
+            <TodoItem
+              currTodo={currTodo}
+              index={index}
+              actions={actions} />
           </li>
         ))}
       </ul>
